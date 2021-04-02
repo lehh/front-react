342976f31cdccc3bf2c1c15f6679006eb5560242
Alessandra <aleterre10@gmail.com>
2021-04-02 16:33:10 -0300

    Added support for env variables

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,57 +1,69 @@
-import path from "path";
+import path from 'path';
+import { config as loadEnv } from 'dotenv';
 import {
   HotModuleReplacementPlugin,
+  DefinePlugin,
   Configuration as WebpackConfiguration,
-} from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ESLintPlugin from "eslint-webpack-plugin";
+} from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
 
 interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
+const getEnvKeys = (): Record<string, string> => {
+  const env = loadEnv().parsed;
+
+  if (!env) {
+    return {};
+  }
+
+  return Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {} as Record<string, string>);
+};
+
 const config: Configuration = {
-  mode: "development",
+  mode: 'development',
   output: {
-    publicPath: "/",
+    publicPath: '/',
   },
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
           },
         },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
     new HotModuleReplacementPlugin(),
     new ESLintPlugin({
-      extensions: ["js", "jsx", "ts", "tsx"],
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
     }),
+    new DefinePlugin(getEnvKeys()),
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "build"),
+    contentBase: path.join(__dirname, 'build'),
     historyApiFallback: true,
-    port: 4000,
+    port: 4001,
     open: true,
     hot: true,
   },
